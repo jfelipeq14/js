bbdc199ee0d95a8e2a6e4b34b27cc9bfe47040e5
Andrey0108 <alanandrey234@gmail.com>
2024-04-05 07:12:28 -0500

    ejercicio numero 1

---
+++ Andrey/index.js
@@ -0,0 +1,12 @@
+function devolverMayor(numeroUno, numeroDos) {
+    if (numeroUno > numeroDos) {
+        return numeroUno
+    } else {
+        numeroDos
+    }
+}
+
+console.log(devolverMayor(3, 6));
+console.log(devolverMayor(5, 2));
+console.log(devolverMayor(8, 8));
+
