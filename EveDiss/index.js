e93b2c16f79b3f85923df194b4ff3f9259ae1a8f
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2024-04-05 07:11:07 -0500

    Ejercicio 1

---
+++ EveDiss/index.js
@@ -0,0 +1,11 @@
+function devolverMayor(numUno, numDos) {
+    if (numUno > numDos) {
+        return numUno
+    }else{
+        return numDos
+    }
+}
+
+console.log(devolverMayor(3,6))
+console.log(devolverMayor(5,4))
+console.log(devolverMayor(3,3))
