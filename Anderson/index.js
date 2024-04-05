110cd84c6a128943a694879962ff5f94283216d7
Andersson <cartagenahianderson@gmail.com>
2024-04-05 07:41:31 -0500

    ejercicio 1

---
+++ Anderson/index.js
@@ -0,0 +1,12 @@
+function devolverMayor(numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1 
+        
+        
+    } else {
+        return numero2
+    }
+}
+console.log(devolverMayor(3,6))       
+console.log(devolverMayor(5,2))       
+console.log(devolverMayor(8,8))       
