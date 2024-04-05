b9e874201899510fc670e5401fea52ba03b56fe8
Anglasmac <anglamarcet@gmail.com>
2024-04-05 09:38:41 -0500

    metodos

---
+++ AngelaM/index.js
@@ -0,0 +1,30 @@
+//ejercicio 1
+// function devolvermayor(numuno,numdos){
+//     if(numuno>numdos){
+//         return numuno
+//     }else{
+//         return numdos
+//     }
+// }
+// console.log(devolvermayor(3,6))
+// console.log(devolvermayor(5,2))
+// console.log(devolvermayor(8,8))
+
+// ejercicio 2
+
+// const parImpar = (numero) =>{
+//     if(numero  % 2===0){
+//         return "es par"
+//     }else {
+//         return "no es par"
+//     }
+// }
+// console.log(parImpar(3));
+
+// 24- Extraer la informacion de un formulario con el evento submit y validar que no este vacia para realizar un login simple donde se muestre la informacion 
+// del usuario logeado en una alerta, validando la informacion de un array de objetos:
+// users = [
+//     {username: "root" password: "root"},
+//     {username: "nico" password: "123"},
+//     {username: "name" password: "098"},
+// ]
