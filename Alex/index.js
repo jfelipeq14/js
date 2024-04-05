e427f1853250d8410f2e6a187509d086d582c86f
Bryan0813 <brayansanchez0813@gmail.com>
2024-04-05 07:12:51 -0500

    Ejercicio 1

---
+++ Alex/index.js
@@ -0,0 +1,11 @@
+function Mayor(Num1,Num2) {
+    if (Num1>Num2) {
+        return Num1
+    } else {
+        return Num2
+    }
+}
+
+console.log(Mayor(3,6))
+console.log(Mayor(5,2))
+console.log(Mayor(8,8))
